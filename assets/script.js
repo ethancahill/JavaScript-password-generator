47cfb107c9947bf08f67d82dbd087b3302761d69
Ethan Cahill <andersmith29@gmail.comhtml lang=en  head    meta charset=UTF-8 /  /head  body    p      I LOVE PANDASgit config --global user.email andersmith29@gmail.com>
2022-03-11 19:25:16 -0500

    created password length function

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,33 @@
 // Assignment code here
+var createdpassword = {
+  length: function() {
+    var passlength = prompt("How long would you like your password to be? Please enter a value between 8 and 128")
+    var numpasslength = parseInt(passlength)
+    if (numpasslength < 8 || numpasslength > 128){
+      alert("Please enter a value between 8 and 128");
+      createdpassword.length()
+    } else {
+      return numpasslength
+    }  
+  },
+  uppercase: "",
+  lowercase: "",
+  numeric: "",
+  special: "",
+}
 
+
+
+createdpassword.length()
+console.log ("The password length selected was " + createdpassword.length())
+  
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = GeneratePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -15,3 +36,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
