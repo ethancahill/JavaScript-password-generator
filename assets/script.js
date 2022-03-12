537f7d04b328c513bc29a0aacc448b518df20f98
Ethan Cahill <andersmith29@gmail.comhtml lang=en  head    meta charset=UTF-8 /  /head  body    p      I LOVE PANDASgit config --global user.email andersmith29@gmail.com>
2022-03-11 20:51:20 -0500

    added boolean values for characters

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,39 @@
 // Assignment code here
 var createdpassword = {
   length: function() {
-    var passlength = prompt("How long would you like your password to be? Please enter a value between 8 and 128")
-    var numpasslength = parseInt(passlength)
+    var passlength = prompt("How long would you like your password to be? Please enter a value between 8 and 128");
+    var numpasslength = parseInt(passlength);
     if (numpasslength < 8 || numpasslength > 128){
       alert("Please enter a value between 8 and 128");
       createdpassword.length()
     } else {
       return numpasslength
-    }  
+  } 
   },
-  uppercase: "",
-  lowercase: "",
-  numeric: "",
-  special: "",
-}
+
+  uppercase: function(){
+    var upper = confirm("Do you want your password to include uppercase letters?")
+    return upper
+  },
+
+  lowercase: function() { 
+    var low = confirm("Do you want your password to contain lowercase letters?")
+    return low
+  },
+
+  numeric: function() {
+    var num = confirm("Do you want your password to contain numeric values?")
+    return num
+  },
+
+  special: function() { 
+    var spec = confirm("Do you want your password to contain special characters?")
+    return spec
+},
+
+};
 
 
-
-createdpassword.length()
-console.log ("The password length selected was " + createdpassword.length())
   
 
 // Get references to the #generate element
@@ -30,7 +44,10 @@ function writePassword() {
   var password = GeneratePassword();
   var passwordText = document.querySelector("#password");
 
+
   passwordText.value = password;
+
+
 
 }
 
@@ -38,3 +55,4 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
+console.log(createdpassword.length())
