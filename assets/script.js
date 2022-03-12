66f57d029e85964b7b6ef100bee8ac0cef3c0b47
Ethan Cahill <andersmith29@gmail.comhtml lang=en  head    meta charset=UTF-8 /  /head  body    p      I LOVE PANDASgit config --global user.email andersmith29@gmail.com>
2022-03-11 22:33:33 -0500

    wrote variables for generator

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,28 @@
 // Assignment code here
+const lwcalphabet = "abcdefghijklmnopqrstuvwxyz";
+const randomlwcCharacter = lwcalphabet[Math.floor(Math.random() * lwcalphabet.length)];
+const upcalphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const randomupcCharacter = upcalphabet[Math.floor(Math.random() * upcalphabet.length)];
+const randomNumericCharacter = Math.floor(math.random() * 10);
+const specCharacter = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+var passwordoptions = [randomNumericCharacter, randomlwcCharacter, randomupcCharacter, specCharacter];
+var arraylength = passwordoptions.length;
+var randomArrayValue = Math.floor(Math.random() * arraylength)
+
+
+  var passwordlength = function() {
+  var passlength = prompt("How long would you like your password to be? Please enter a value between 8 and 128");
+  var numpasslength = parseInt(passlength);
+  if (numpasslength < 8 || numpasslength > 128){
+    alert("Please enter a value between 8 and 128");
+    createdpassword.length()
+  } else {
+    return numpasslength
+} 
+}
 var createdpassword = {
-  length: function() {
-    var passlength = prompt("How long would you like your password to be? Please enter a value between 8 and 128");
-    var numpasslength = parseInt(passlength);
-    if (numpasslength < 8 || numpasslength > 128){
-      alert("Please enter a value between 8 and 128");
-      createdpassword.length()
-    } else {
-      return numpasslength
-  } 
-  },
+  length: passwordlength(),
 
   uppercase: function(){
     var upper = confirm("Do you want your password to include uppercase letters?")
@@ -33,6 +46,27 @@ var createdpassword = {
 
 };
 
+var Declarations = function() {
+  if (!createdpassword.uppercase()){
+    passwordoptions.splice(2,1)
+  };
+  if (!createdpassword.lowercase()){
+    passwordoptions.splice(1,1)
+  };
+  if (!createdpassword.special()){
+    passwordoptions.splice(3,1)
+  };
+  if (!createdpassword.numeric()){
+    passwordoptions.splice(0,1)
+  };
+};
+var PasswordGenerator = function(){
+  for (var i = 0; i < createdpassword.length; i++) {
+    console.log(passwordoptions[randomArrayValue])
+  }  
+  }
+
+
 
   
 
@@ -47,12 +81,9 @@ function writePassword() {
 
   passwordText.value = password;
 
-
-
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
-console.log(createdpassword.length())
+
